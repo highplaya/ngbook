b9960a1c17a81e17c2ccfb8278d642f3940d670d
ihor <ihorhabshiy@gmail.com>
2016-02-07 16:21:33 +0200

    username validation

--- form-validation/app.js
+++ form-validation/app.js
@@ -6,4 +6,22 @@ angular.module('form', [])
 	$scope.signupForm = function() {
 		console.log(signup_form);
 	}
-}]);
+}])
+.directive('ensureUnique', function($http) {
+	require: 'ngModel',
+	link: function(scope, ele, attrs, c) {
+		scope.$watch(attrs.ngModel, function(n) {
+			if(!n) return;
+
+			$http({
+				method: 'POST',
+				url: '/api/check/' + attrs.ensureUnique,
+				data: { 'field': attrs.ensureUnique }
+			}).success(function(data) {
+				c.$setValidity('unique', data.isUnique);
+			}).error(function(data) {
+				c.$setValidity('unique', false);
+			})
+		})
+	}
+});
